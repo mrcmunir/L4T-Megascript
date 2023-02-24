b8876e83ec55cc80fb9d974b50a88401e1c3f06d
Carlos Estrague <maximu@gmail.com>
2023-02-24 15:58:02 +0100

    retest

--- scripts/vulkan-headers.sh
+++ scripts/vulkan-headers.sh
@@ -2,12 +2,12 @@
 
 sudo apt install cmake gcc -y || error "Could not install packages"
 cd /tmp || error "Could not move to /tmp folder"
-rm -rf ./Vulkan-headers
-git clone https://github.com/KhronosGroup/Vulkan-headers.git
-cd Vulkan-headers
+rm -rf ./Vulkan-Headers
+git clone https://github.com/KhronosGroup/Vulkan-Headers.git
+cd Vulkan-Headers
 mkdir build 
 cd build
 cmake ..
 make -j$(nproc)
 sudo make install || error "Make install failed"
-rm -rf /tmp/Vulkan-headers
+rm -rf /tmp/Vulkan-Headers
